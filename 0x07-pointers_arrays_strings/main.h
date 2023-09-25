a6c8ffaf8bc365ac38677b725af98430512a5fc4
ahmedemadsw <ahmed.frln19999@gmail.com>
2023-09-25 13:31:39 -0700

    a

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -4,6 +4,7 @@
 #include <stdio.h>
 int _putchar(char c);
 char *_memset(char *s, char b, unsigned int n);
+void simple_print_buffer(char *buffer, unsigned int size)
 char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
