dc7a6442f1b1bb68c0585869401c2ac49b317788
ahmedemadsw <ahmed.frln19999@gmail.com>
2023-10-04 11:23:14 -0700

    a

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Prints the number of arguments passed to it.
+ * @argc: The number of arguments supplied to the program.
+ * @argv: An array of pointers to the arguments.
+ *
+ * Return: Always 0.
+ */
+int main(int argc, char *argv[])
+{
+	int prod, x, y;
+
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	x = atoi(argv[1]);
+	y = atoi(argv[2]);
+	prod = x * y;
+
+	printf("%d\n", prod);
+
+	return (0);
+}
